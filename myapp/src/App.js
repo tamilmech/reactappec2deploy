2846906895c84ca00f219e3f8865aa2a6cf453b0
tamilmech <85125265+tamilmech@users.noreply.github.com>
2023-11-08 10:20:15 +0530

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         after hjhj
+         tamilselvan
         </p>
         <a
           className="App-link"
